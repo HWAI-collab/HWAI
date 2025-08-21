fe11885df4aa06a4767da1ab41d696621041e8fc
Jarred Muller <jarredmuller@Jarreds-MacBook-Pro.local>
2025-08-21 17:02:04 +1000

    Add View Details buttons back to repository cards

--- app.js
+++ app.js
@@ -142,6 +142,7 @@ function createRepoCard(repo, readme) {
         ` : ''}
         
         <div class="card-footer">
+            <button class="btn-primary" onclick="expandReadme('${repo.name}')">View Details</button>
             <button class="btn-primary" onclick="requestAccess('${repo.name}')">Request Source Code</button>
             <button class="btn-primary" onclick="downloadReadme('${repo.name}')">Download README</button>
         </div>
